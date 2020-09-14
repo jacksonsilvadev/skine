079b1a1f0367b657bd99f85889818a54f86cbc2d
Jackson Silva <jacksonsilvadev@gmail.com>
2020-09-14 10:53:26 -0300

    Fix error jwt middleware

--- providers/default/auth/index.js
+++ providers/default/auth/index.js
@@ -1,19 +1,31 @@
 const axios = require('axios').default;
+const UnauthorizedError = require('../../../errors/unauthorized-error');
+
 
 const config = require('../../../config');
 
 const AUTH_API_HOST = `${config.auth.api}`;
 
 const authenticate = async (authToken) => {
-  if (!authToken) {
-    return null;
+  try {
+    if (!authToken) {
+      return null;
+    }
+  
+    const result = await axios.post(`${AUTH_API_HOST}validate-user`, {
+      authToken,
+    });
+    
+    return result.data;
+  } catch (error) {
+    const message = error.response.data.message
+    if(message === 'jwt expired'){
+      throw new UnauthorizedError(message)
+    }
+
+    return error
   }
-
-  const result = await axios.post(`${AUTH_API_HOST}validate-user`, {
-    authToken,
-  });
-
-  return result.data;
+ 
 };
 
 module.exports = {
